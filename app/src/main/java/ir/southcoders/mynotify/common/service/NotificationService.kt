346242ac51d5b8022301e3d13fd8c5221ebba259
farzad akhondzadeh <farzad119@gmail.com>
2017-11-16 01:58:16 +0330

    added mvp packages - added NotificationService.kt class and added to manifest

---
+++ app/src/main/java/ir/southcoders/mynotify/common/service/NotificationService.kt
@@ -0,0 +1,62 @@
+package ir.southcoders.mynotify.common.service
+
+import android.app.Notification
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.service.notification.NotificationListenerService
+import android.service.notification.StatusBarNotification
+import android.support.v4.content.LocalBroadcastManager
+import android.util.Log
+
+import java.io.ByteArrayOutputStream
+
+/**
+ * Created by Farzad on 11/15/2017.
+ */
+
+class NotificationService : NotificationListenerService() {
+    internal var context: Context
+
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+    }
+
+    override fun onNotificationPosted(sbn: StatusBarNotification) {
+        val pack = sbn.packageName
+        var ticker = ""
+        if (sbn.notification.tickerText != null) {
+            ticker = sbn.notification.tickerText.toString()
+        }
+        val extras = sbn.notification.extras
+        val title = extras.getString("android.title")
+        val text = extras.getCharSequence("android.text")!!.toString()
+        val id1 = extras.getInt(Notification.EXTRA_SMALL_ICON)
+        val id = sbn.notification.largeIcon
+
+
+        Log.i("Package", pack)
+        Log.i("Ticker", ticker)
+        Log.i("Title", title)
+        Log.i("Text", text)
+
+        val msgrcv = Intent("Msg")
+        msgrcv.putExtra("package", pack)
+        msgrcv.putExtra("ticker", ticker)
+        msgrcv.putExtra("title", title)
+        msgrcv.putExtra("text", text)
+        if (id != null) {
+            val stream = ByteArrayOutputStream()
+            id.compress(Bitmap.CompressFormat.PNG, 100, stream)
+            val byteArray = stream.toByteArray()
+            msgrcv.putExtra("icon", byteArray)
+        }
+        LocalBroadcastManager.getInstance(context).sendBroadcast(msgrcv)
+    }
+
+    override fun onNotificationRemoved(sbn: StatusBarNotification) {
+        Log.i("Msg", "Notification Removed")
+    }
+}
